5cc7ef73b5af2fb089749d6065e8732a6fd13fed
Navjot Kaur <kaur97351@gmail.com>
2022-04-23 07:38:13 -0500

    adding function to accept all the user requests in oauth workflow

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,22 @@
 const functions = require("firebase-functions");
 const cors = require("cors");
 const {canvasRequest} = require("./canvasRequest");
+const express = require('express');
+const app = express();
+
+//this function receives all the requests coming in oauth flow and logs them out and also prints then 
+app.get('/:allRequests',(req,res) =>{
+  functions.logger.log(req.params.allRequests);
+  return res.status(200).send(req.params.allRequests)
+});
+
+exports.app = functions.https.onRequest(app);
 
 module.exports.oauthStart = functions.https.onRequest((req, res) => {
-    functions.logger.log(req)
+    functions.logger.log(req.body)
     res.send(JSON.stringify(req.body, null, 4))
 })
+
 
 module.exports.canvasCors = functions.https.onRequest((req, res) => {
   cors()(req, res, () => {
@@ -14,6 +25,7 @@ module.exports.canvasCors = functions.https.onRequest((req, res) => {
         .then(value => {
           res.status(200);
           res.send(value);
+          
         })
         .catch(reason => {
           res.status(reason.statusCode);
@@ -21,4 +33,8 @@ module.exports.canvasCors = functions.https.onRequest((req, res) => {
         })
   });
 });
+
+
+
+
 
